92e46f23421ebfce3a62d6442265ced0cab0ab84
krishnapriya <krishnapriyamkkp@gmail.com>
2025-04-05 17:42:01 +0530

    updated score at 541pm

--- frontend/src/Score.js
+++ frontend/src/Score.js
@@ -3,15 +3,15 @@ import "./score.css"; // Import CSS file
 
 const Score = () => {
   const scores = [
-    { position: 1, department: "MCA", points: 178 },
-    { position: 2, department: "PHD", points: 131 },
-    { position: 3, department: "Electronics Engineering", points: 62 },
-    { position: 4, department: "Civil Engineering", points: 59 },
-    { position: 5, department: "Electrical Engineering", points: 41 },
-    { position: 6, department: "Mechanical Engineering", points: 38 },
-    { position: 7, department: "Architecture", points: 27 },
+    { position: 1, department: "MCA", points: 191 },
+    { position: 2, department: "PHD", points: 136 },
+    { position: 3, department: "Electronics Engineering", points: 79 },
+    { position: 4, department: "Civil Engineering", points: 74 },
+    { position: 5, department: "Mechanical Engineering", points: 47 },
+    { position: 6, department: "Architecture", points: 46 },
+    { position: 7, department: "Electrical Engineering", points: 41 },
     { position: 8, department: "MBA", points: 12 },
-    { position: 9, department: "Computer Science Engineering", points: 0 },
+    { position: 9, department: "Computer Science Engineering", points: 1 },
   ];
 
   return (
